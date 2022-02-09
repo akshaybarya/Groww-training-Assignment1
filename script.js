aa2216138aca6863f60942a5a7a6add5352e580e
Akshay Kumar Barya <akshaykumarbarya@Akshays-MacBook-Pro.local>
2022-02-09 18:51:14 +0530

    Completed 1st Assignment

---
+++ script.js
@@ -0,0 +1,131 @@
+"use strict";
+
+function CustomAlert() {
+  this.render = function (message, focus) {
+    let win_w = window.innerWidth;
+    let win_h = window.innerHeight;
+
+    const dialogoverlay = document.getElementById("dialogoverlay");
+    const dialogbox = document.getElementById("dialogbox");
+
+    dialogoverlay.style.display = "block";
+    dialogoverlay.style.height = win_h + "px";
+    dialogbox.style.display = "flex";
+
+    document.getElementById("dialogboxbody").innerHTML = message;
+    document.getElementById("dialogboxfootbutton").onclick = (e) =>
+      Alert.setFocus(focus);
+  };
+
+  this.setFocus = function (focus) {
+    const dialogoverlay = document.getElementById("dialogoverlay");
+    const dialogbox = document.getElementById("dialogbox");
+
+    document.getElementById(focus).focus();
+
+    dialogoverlay.style.display = "none";
+    dialogbox.style.display = "none";
+  };
+}
+
+const Alert = new CustomAlert();
+
+document.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const errors = [];
+
+  const fname = document.getElementById("first-name").value;
+  if (fname.indexOf(" ") >= 0) {
+    errors.push("first-name");
+  }
+
+  const lname = document.getElementById("last-name").value;
+  if (lname.indexOf(" ") >= 0) {
+    errors.push("last-name");
+  }
+
+  if (
+    !document.querySelector('input[name="gender"]:checked') ||
+    document.querySelector('input[name="gender"]:checked').length <= 0
+  ) {
+    errors.push("gender");
+  }
+
+  // const gender = document.querySelector('input[name="gender"]:checked').value;
+
+  let maretialStatus;
+  if (
+    !document.querySelector('input[name="maretial-status"]:checked') ||
+    document.querySelector('input[name="maretial-status"]:checked').length <= 0
+  ) {
+    errors.push("maretial-status");
+  } else {
+    maretialStatus = document.querySelector(
+      'input[name="maretial-status"]:checked'
+    ).value;
+  }
+  if (maretialStatus === "married") {
+    const spouce = document.getElementById("spouce-name").value;
+    if (spouce.indexOf(" ") >= 0) {
+      errors.push("spouce-name");
+    }
+  }
+
+  if (
+    !document.querySelector('input[name="check-box"]:checked') ||
+    document.querySelector('input[name="check-box"]:checked').length <= 0
+  ) {
+    errors.push("check-box");
+  }
+
+  if (errors.length === 0) sucess();
+  else dispatchErrors(errors);
+});
+
+function dispatchErrors(errors) {
+  let message = "";
+
+  for (let error of errors) {
+    switch (error) {
+      case "first-name": {
+        message = message + "Enter First Name without white Spaces.";
+        break;
+      }
+      case "last-name": {
+        message = message + "Enter Last Name without white Spaces.";
+        break;
+      }
+      case "gender": {
+        message = message + "Please select a gender.";
+        break;
+      }
+      case "maretial-status": {
+        message = message + "Please select your Maretial status.";
+        break;
+      }
+      case "check-box": {
+        message = message + "Please Accept terms & conditions.";
+        break;
+      }
+      default:
+        break;
+    }
+    message = message + "<br>";
+  }
+
+  Alert.render(message, errors[0]);
+}
+
+function sucess() {
+  const table = document.getElementById("table");
+  const thankYou = document.getElementById("thank-you");
+  table.style.display = "none";
+  thankYou.style.display = "block";
+}
+
+function alterMaretialStatus(enable) {
+  const spouce = document.getElementById("spouse-row");
+
+  if (enable === true) spouce.style.display = "table-row";
+  else spouce.style.display = "none";
+}
